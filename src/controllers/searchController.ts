a61b43a8a110d88200ef55a99f25f3cfd008a60b
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-05 05:58:57 -0300

    Adicionando o controlador de busca e a página 404 e finalizando o projeto

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObjects';
+
 export const search = (req: Request, res: Response) => {
-    
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
